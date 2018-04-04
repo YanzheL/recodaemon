79b25daadc3cdcc16dbbb5f7bc353c68f066b468
YanzheL <lee.yanzhe@yanzhe.org>
2018-04-04 21:55:22 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 script_dir=$(cd `dirname $0`; pwd)
+JAVA_HOME=/usr/lib/jvm/jdk-9.0.4
 cd ${script_dir}
-java -Xmx1000m -Dorg.tensorflow.NativeLibrary.DEBUG=1 -Djava.library.path=src/main/resources/lib/tensorflow/linux-x86_64:src/main/resources/lib/processors/linux-x86_64 -jar target/*.jar
+$JAVA_HOME/bin/java -Xmx1000m -Dorg.tensorflow.NativeLibrary.DEBUG=1 -Djava.library.path=src/main/resources/lib/processors/linux-x86_64 -jar target/*.jar
